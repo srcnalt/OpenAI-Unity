bd364f18cc026bb759b732da71f061a2027e3cd3
Sercan Altundas <srcnalt@users.noreply.github.com>
2023-01-19 15:31:07 +0200

    feat: auth file check, json serialization settings

--- Runtime/Configuration.cs
+++ Runtime/Configuration.cs
@@ -1,29 +1,38 @@
 using System;
 using System.IO;
+using UnityEngine;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 
 namespace OpenAI
 {
     public class Configuration
     {
-        public string ApiKey { get; }
-        public string Organization { get; }
+        public Auth Auth { get; }
+        
+        /// Used for serializing and deserializing PascalCase request object fields into snake_case format for JSON. Ignores null fields when creating JSON strings.
+        private readonly JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings()
+        {
+            NullValueHandling = NullValueHandling.Ignore, 
+            ContractResolver = new DefaultContractResolver()
+            {
+                NamingStrategy = new CustomNamingStrategy()
+            }
+        };
         
         public Configuration()
         {
-            var path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-            var json = File.ReadAllText($"{path}/.openai/auth.json");
-            var auth = JsonConvert.DeserializeObject<JToken>(json);
+            var userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            var authPath = $"{userPath}/.openai/auth.json";
 
-            if (auth != null)
+            if (File.Exists(authPath))
             {
-                ApiKey = auth["api_key"]!.Value<string>();
-                Organization = auth["organization"]!.Value<string>();
+                var json = File.ReadAllText(authPath);
+                Auth = JsonConvert.DeserializeObject<Auth>(json, jsonSerializerSettings);
             }
             else
             {
-                throw new Exception("auth.json could not be found.");
+                Debug.LogError($"auth.json does not exist. Please check https://github.com/srcnalt/OpenAI-Unity#saving-your-credentials");
             }
         }
     }
