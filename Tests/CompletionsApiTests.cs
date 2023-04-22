c652d2a3bddc22cb991e2d92aa4a604858c85b5d
Sercan Altundas <srcnalt@users.noreply.github.com>
2023-04-22 19:37:22 +0300

    test: chat and text completion stream tests

--- Tests/CompletionsApiTests.cs
+++ Tests/CompletionsApiTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OpenAI.Tests
@@ -8,7 +9,7 @@ namespace OpenAI.Tests
         private OpenAIApi openai = new OpenAIApi();
         
         [Test]
-        public async Task Create_Completion()
+        public async Task Create_Text_Completion()
         {
             var req = new CreateCompletionRequest
             {
@@ -19,6 +20,36 @@ namespace OpenAI.Tests
             };
             var res = await openai.CreateCompletion(req);
             Assert.NotNull(res);
+        }
+        
+        [Test]
+        public async Task Create_Text_Completion_Stream()
+        {
+            bool responseReceived = false;
+            float timeout = 5;
+            float time = 0;
+            
+            var req = new CreateCompletionRequest
+            {
+                Model = "text-davinci-003",
+                Prompt = "Say this is a test",
+                MaxTokens = 7,
+                Temperature = 0,
+                Stream = true
+            };
+            
+            openai.CreateCompletionAsync(req, null, () => 
+            {
+                responseReceived = true;
+            }, new CancellationTokenSource());
+            
+            while (!responseReceived && time < timeout)
+            {
+                await Task.Delay(100);
+                time += 0.1f;
+            }
+            
+            Assert.IsTrue(responseReceived);
         }
     }
 }
