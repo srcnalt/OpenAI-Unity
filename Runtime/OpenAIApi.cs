59b26ad2fd54fc6b8ec0066512f1c24623ccf952
气泡缓释胶囊 <japanztp@gmail.com>
2024-04-05 16:45:23 +0800

    Update OpenAIApi.cs

--- Runtime/OpenAIApi.cs
+++ Runtime/OpenAIApi.cs
@@ -36,7 +36,7 @@ namespace OpenAI
         }
 
         /// OpenAI API base path for requests.
-        private const string BASE_PATH = "https://api.openai.com/v1";
+        private string BASE_PATH = "https://api.openai.com/v1";
 
         public OpenAIApi(string apiKey = null, string organization = null,string basePath = null)
         {
